7dfa87d7818b929337f9f854b684220c2f03f200
Robert Irwin <t979140@spark.co.nz>
2024-08-07 13:41:23 +1200

    add pull request data for myself

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,95 @@
 import { Hono } from "hono";
 import type { FC } from "hono/jsx";
+import { serveStatic } from "hono/bun";
+
+import type PullRequestData from "../t979140.json";
 
 const app = new Hono();
 
-const Top: FC<{ messages: string[] }> = (props: { messages: string[] }) => {
+app.use("/favicon.ico*", serveStatic({ root: "./favicon.ico" }));
+app.use("/missing.css/*", serveStatic({ path: "../node_modules/missing.css/" }));
+
+const PullRequests: FC<{ pullRequests: typeof PullRequestData }> = ({
+  pullRequests,
+}) => (
+  <div>
+    {pullRequests.map((pr) => (
+      <details>
+        <summary>
+          {pr.pullRequestId} {pr.title}
+        </summary>
+        <table>
+          <tbody>
+            <tr>
+              <td>Status</td>
+              <td>{pr.status}</td>
+            </tr>
+            <tr>
+              <td>Created</td>
+              <td>{new Date(pr.creationDate).toISOString()}</td>
+            </tr>
+            <tr>
+              <td>Closed</td>
+              <td>{new Date(pr.closedDate).toISOString()}</td>
+            </tr>
+          </tbody>
+        </table>
+        <ul>
+          {Object.entries(pr).map(([key, value]) => {
+            if (
+              value == null ||
+              typeof value == "string" ||
+              typeof value == "number"
+            ) {
+              return (
+                <li>
+                  {key}: {value}
+                </li>
+              );
+            } else if (Array.isArray(value)) {
+              return <li>{key}: [Array]</li>;
+            } else {
+              <li>
+                {key}: {Object}
+              </li>;
+            }
+          })}
+        </ul>
+        <ul>{pr.completionOptions}</ul>
+      </details>
+    ))}
+  </div>
+);
+
+const Top: FC<{ pullRequests: typeof PullRequestData }> = (props) => {
   return (
-    <main>
-      <h1>Hello Hono!</h1>
-      <ul>
-        {props.messages.map((message) => {
-          return <li>{message}!!</li>;
-        })}
-      </ul>
-    </main>
+    <html>
+      <head>
+        <title>User Pull Requests</title>
+      </head>
+      <body>
+        <main>
+          <h1>Pull Requests</h1>
+          <PullRequests pullRequests={props.pullRequests} />
+        </main>
+      </body>
+    </html>
   );
 };
 
-app.get("/", (c) => {
-  const messages = ["hello", "world"];
-  return c.html(<Top messages={messages} />);
+app.get("/", async (c) => {
+  const file = Bun.file("./t979140.json");
+  const json = await file.json();
+  return c.html(<Top pullRequests={json} />);
+});
+
+app.get("/prs", async (c) => {
+  const file = Bun.file("./t979140.json");
+  const json = await file.json();
+  return c.html(<PullRequests pullRequests={json} />);
 });
 
 export default {
   port: 4000,
-  fetch: app.fetch, 
+  fetch: app.fetch,
 };
